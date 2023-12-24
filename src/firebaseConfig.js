8bc43039bdad03c9869775dba10168944d31f1eb
Sarah Kulbersh <129111289+SarahKulbersh@users.noreply.github.com>
2023-12-24 15:09:07 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8XW6bPdqL3YtCWlJJzjj2Ca9VR1Kv1aE",
-  authDomain: "acheinu-1c232.firebaseapp.com",
-  projectId: "acheinu-1c232",
-  storageBucket: "acheinu-1c232.appspot.com",
-  messagingSenderId: "241587821835",
-  appId: "1:241587821835:web:b88c90f9afb1f641f5160e",
-  measurementId: "G-9LSCJLMSKM"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
